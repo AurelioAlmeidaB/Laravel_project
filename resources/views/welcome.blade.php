0dd456bdb6d9e9ef344d08a1c6f22487d7d42538
AurelioAlmeidaB <168114923+AurelioAlmeidaB@users.noreply.github.com>
2026-06-12 01:16:14 -0300

    teste se o deploy esta indo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <div id="search-container" class="col-md-12">
-    <h1>Busque um evento</h1>
+    <h1>Busque um event1o</h1>
     <form action="/" method="GET">
         <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
     </form>
